871f3bf93465bdc722072a61a66b797a542a3f07
Felipe Andrade <flpandrade2011@gmail.com>
2020-07-09 17:40:11 -0300

    Iniciante

---
+++ INICIANTE/2653.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+  string joia;
+  set <string> joias;
+
+  while(cin >> joia) {
+    joias.insert(joia);
+  }
+
+  cout << joias.size() << endl;
+}
